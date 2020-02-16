9370758d4d0c28c451a8cc1ba70de191bca5b932
Unknown <Duncan.legoff@gmail.com>
2020-02-16 23:43:23 +0100

    adding cache data stocking

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const apiKeys = require('./apiKey.js')
+const data = require('./data.js')
 const fetch = require('isomorphic-unfetch')
 const Mixer = require('@mixer/client-node')
 const clientMixer = new Mixer.Client(new Mixer.DefaultRequestRunner())
@@ -17,20 +18,63 @@ clientMixer.use(new Mixer.OAuthProvider(clientMixer, {
 }))
 
 app.get('/', async function (req, res) {
-    let mixerData = await getMixerBaseData()
-    // let twitchData = await getTwitchBaseData()
-    getYouTubeBaseData();
-    res.json(mixerData)
+    // let mixerData = await getMixerBaseData()
+    let twitchData = await getTwitchBaseData()
+    // getYouTubeBaseData();
+    res.json(twitchData)
 })
-app.get('/kek', function (req, res) {
-    res.send('Hello World! kek')
+
+app.get('/topgames', async function (req, res) {
+    let twitchData = await getTwitchBaseData(0)
+    res.json(twitchData)
 })
+
+app.get('/topgames/:offset', async function (req, res) {
+    let offset = req.params.offset
+    if ((offset % 100) === 0){
+        if (Date.now() - data.topgames[offset].timestamp > 30000){
+            console.log('GENERATING NEW DATA')
+            let twitchData = await getTwitchBaseData(offset)
+            res.json(twitchData)
+        } else {
+            console.log('DATA ALREADY STORED')
+            res.json(data.topgames[offset])
+        }
+    } else {
+        res.send('ERROR IN OFFSET NUMBER')
+    }
+})
+
+async function getTwitchBaseData(offset) {
+    let games = await fetch('https://api.twitch.tv/kraken/games/top?limit=100&offset=' + offset + '&client_id=' + apiKeys.twitch,
+        {
+            method: 'GET',
+            headers: {
+                'Client-ID': apiKeys.twitch,
+                'Accept': 'application/vnd.twitchtv.v5+json'
+            },
+            mode: 'cors',
+            cache: 'default'
+        });
+    let gamesJson = await games.json()
+    if (data.topgames[offset] === undefined){
+        data.topgames[offset] = {
+            data: [],
+            timestamp: 0
+        }
+    }
+    data.topgames[offset].data = gamesJson.top;
+    data.topgames[offset].timestamp = Date.now();
+
+    return data.topgames[offset]
+}
 
 app.get('/auth/twitch', async function (req, res) {
     let userAccess = await fetch('https://id.twitch.tv/oauth2/token?client_id=' + apiKeys.twitch + '&client_secret=' + apiKeys.secretTwitch + '&code=' + req.query.code + '&grant_type=authorization_code&redirect_uri=http://localhost:3000/auth/twitch', {
         method: "POST"
     })
     let userAccessJson = await userAccess.json();
+    console.log(userAccessJson)
     res.json(userAccessJson)
 })
 
@@ -61,20 +105,7 @@ async function getYouTubeBaseData() {
     //   });
 }
 
-async function getTwitchBaseData() {
-    let games = await fetch('https://api.twitch.tv/kraken/games/top?client_id=' + apiKeys.twitch,
-        {
-            method: 'GET',
-            headers: {
-                'Client-ID': apiKeys.twitch,
-                'Accept': 'application/vnd.twitchtv.v5+json'
-            },
-            mode: 'cors',
-            cache: 'default'
-        });
-    let gamesJson = await games.json()
-    return gamesJson
-}
+
 
 app.listen(3030, function () {
     console.log('Example app listening on port 3030!')
---
+++ data.js
@@ -0,0 +1,8 @@
+var topgames = {
+    0: {
+        data: [],
+        timestamp: 0
+    }
+}
+
+module.exports.topgames = topgames
